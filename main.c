c65fdaf48326739627a841f50fe19656d8682195
jw2511281 <jw2511281@sookmyung.ac.kr>
2025-10-16 15:35:34 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,17 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int inc(int counter);
+
+
 int main(void)
 {
-    int i;
-    int temp = 1;
+    int i=10;
+    printf("함수 호출 전 i=%d\n", i);
+    i = inc(i);
+    printf("함수 호출 후 i=%d\n", i);
     
-    for(i=0;i<5;i++)
-    {
-     printf("temp = %d\n", temp);
-     temp++;
-     }
- system("PAUSE");
- return 0;
-  
+    system("PAUSE");
+    return 0;
 }
+int inc(int counter)
+{
+    counter++;
+    return counter;
+} 
+ 
+  
